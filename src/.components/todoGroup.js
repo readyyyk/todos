e2b7f6a40ad9d6070aeae67ecf00ddd7c1f88c44
readyyyk <mr.sairexer@gmail.com>
2022-10-23 16:20:34 +0300

    hotfix design

--- src/.components/todoGroup.js
+++ src/.components/todoGroup.js
@@ -5,7 +5,8 @@ import {TrashIcon} from "@primer/octicons-react";
 import Button from "react-bootstrap/Button";
 
 const TodoGroup = ({data, innerData, deleteGroupHandler, addTodo, deleteItem, setStatus}) => {
-
+	let moreThanThree = innerData.length>3?"justify-content-xxl-between":"justify-content-xxl-start"
+	let moreThanTwo = innerData.length>2?"justify-content-xl-between":"justify-content-xl-start"
 	return (
 		<div className="bg-opacity-25 bg-black m-3 mt-4 p-4 rounded-3">
 			<h1 className="fs-2 fw-bold text-primary d-flex justify-content-center align-items-center">
@@ -19,7 +20,7 @@ const TodoGroup = ({data, innerData, deleteGroupHandler, addTodo, deleteItem, se
 				</div>
 			</h1>
 			<TodoForm addTodo={addTodo} groupId={data.id}/>
-			<div className="d-flex justify-content-center justify-content-lg-start justify-content-xl-between flex-wrap mt-3">
+			<div className={"d-flex justify-content-center justify-content-lg-start" + moreThanTwo + " " + moreThanThree + " flex-wrap mt-3"}>
 				{
 					innerData.map( (todoElData, i) =>
 						<TodoItem
